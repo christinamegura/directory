f3acdf251a92ce5eb6bf688af7b3af745ebbab54
christinamegura <52924611+christinamegura@users.noreply.github.com>
2019-08-05 12:40:43 -0400

    Add files via upload

---
+++ src/JSTeachers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JSTeachers = () => [
+    <li key="1">Alex Megura</li>,
+    <li key="2">Sara Jolley</li>,
+    <li key="3">Mary Wheeler</li>,
+];
+
+export default JSTeachers;
